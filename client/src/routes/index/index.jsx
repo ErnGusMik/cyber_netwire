170cbebad31d41eac7784e651a0cd30b8e6c779d
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2025-11-06 17:24:10 +0200

    Added interface highlight

--- client/src/routes/index/index.jsx
+++ client/src/routes/index/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 
 export default function Index() {
     return (
         <div className="homepage">
             <div className="nav">
                 <ul>
-                    <li><a href="">Features</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">More</a></li>
-                    <li><a href="/auth">Log in</a></li>
-                    <li className="nav-emphasis"><a href="/auth">Sign up</a></li>
+                    <li>
+                        <a href="">Features</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">More</a>
+                    </li>
+                    <li>
+                        <a href="/auth">Log in</a>
+                    </li>
+                    <li className="nav-emphasis">
+                        <a href="/auth">Sign up</a>
+                    </li>
                 </ul>
             </div>
             <div className="banner">
@@ -19,19 +29,54 @@ export default function Index() {
                         <h2>Ignite</h2>
                         <h1>Your messaging</h1>
                     </div>
-                    <p>Join Cyber Netwire for a chat experience thats as electrifying as the neon skyline. <br />Secure, fast, and dazzling.</p>
-                    <a href="/auth"><button className="btn"><h3>Join the revolution</h3></button></a>
+                    <p>
+                        Join Cyber Netwire for a chat experience thats as
+                        electrifying as the neon skyline. <br />
+                        Secure, fast, and dazzling.
+                    </p>
+                    <a href="/auth">
+                        <button className="btn">
+                            <h3>Join the revolution</h3>
+                        </button>
+                    </a>
                 </div>
-                <img src="img-placeholder.webp" alt="A banner pic will go here at some point" />
+                <img
+                    src="img-placeholder.webp"
+                    alt="A banner pic will go here at some point"
+                />
             </div>
             <div className="interface">
                 <div className="transition">
-                    <p>/// MODULE._FEATURES_MAIN</p>
+                    <p className="module-name">/// MODULE._FEATURES_MAIN</p>
+                    <h2>Welcome to the Future</h2>
+                    <hr />
                 </div>
-                <h2>Welcome to the Future</h2>
+                <div className="content">
+                    <div className="image">
+                        <p className="module-name primary">/// .SUBMODULE.$INTERFACE</p>
+                        <img src="./interface.png" alt="Image of the product's user interface" />
+                    </div>
+                    <hr />
+                    <div className="text">
+                        <h2>An interface that glows</h2>
+                        <hr />
+                        <p className="tertiary">Navigate with style through our neon-drenched design</p>
+                    </div>
+                </div>
+            </div>
+            <div className="encryption">
+                <p className="module-name tertiary">/// .SUBMODULE.$SECURITY</p>
                 <hr />
-                
+                <div className="content">
+                    <div className="anim-text"></div>
+                    <div className="main">
+                        <h2 className="secondary text-shadow-secondary">Top-tier encryption</h2>
+                        <hr />
+                        <p className="tertiary">Your privacy is our priority: stay secure in the urban jungle</p>
+                    </div>
+                    <div className="anim-text"></div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
